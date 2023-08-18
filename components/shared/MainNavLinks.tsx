cd31f80952c5760e6faf93f2e596e0786bc18dff
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-18 20:15:23 +0800

    Created to use pathname

---
+++ components/shared/MainNavLinks.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { MdAddCircle, MdPersonSearch, MdHome } from "react-icons/md";
+import { BsFillCollectionFill } from "react-icons/bs";
+import { leftNavLinks } from "@/constant";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MainNavLinks = () => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {leftNavLinks.map((link, i: number) => {
+        const isActive =
+          (pathname.includes(link.route) && link.route.length > 1) ||
+          pathname === link.route;
+
+        return (
+          <Link
+            href={link.route}
+            className={`flex items-center gap-3 p-2.5 rounded-lg font-medium ${
+              isActive
+                ? `text-light-1 bg-accent-2 `
+                : `text-dark-1 bg-light-1 hover:bg-light-2`
+            } `}>
+            {i === 0 && <MdHome size={25} />}
+            {i === 1 && <MdAddCircle size={25} />}
+            {i === 2 && <BsFillCollectionFill size={22} />}
+            {i === 3 && <MdPersonSearch size={25} />}
+            {link.label}
+          </Link>
+        );
+      })}
+    </>
+  );
+};
+
+export default MainNavLinks;
