af5fb44a82046befb10959bcf639ba033f7bb9ff
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-19 17:12:07 +0800

    added flex-1

--- app/(root)/top-showjects/page.tsx
+++ app/(root)/top-showjects/page.tsx
@@ -1,6 +1,6 @@
 async function Page() {
   return (
-    <section className="min-h-[200vh] w-full bg-light-2 p-3">
+    <section className="min-h-[200vh] flex-1 bg-light-2 p-3">
       <h1 className="text-xl font-semibold text-dark-1 border-b-[1px] pb-2">
         Top showjects
       </h1>
