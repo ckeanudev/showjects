77396201e7f8c92738811ec12ea96c2f0e4744b4
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-19 08:54:01 +0800

    Implement a followers and following feature but instead it's respect

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -19,6 +19,18 @@ const userSchema = new mongoose.Schema({
       ref: "Showjects",
     },
   ],
+  respectCount: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  respectDevs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
   onboarded: {
     type: Boolean,
     default: false,
