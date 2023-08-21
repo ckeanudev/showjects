40f8717e4a0839bd0341889a2d307945ee7f2005
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-21 09:47:58 +0800

    Focus effect implemented

--- components/cards/ProfileCard.tsx
+++ components/cards/ProfileCard.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import { memo } from "react";
+import { usePathname } from "next/navigation";
 
 interface Props {
   userId: string;
@@ -16,8 +18,13 @@ const ProfileCard = ({
   name,
   username,
 }: Props) => {
+  const pathname = usePathname();
+
   return (
-    <div className="flex items-center gap-4 p-2.5 border rounded-lg hover:bg-light-2">
+    <div
+      className={`flex items-center gap-4 p-2.5 border rounded-lg ${
+        pathname === `/${username}` ? `bg-accent-2` : `hover:bg-light-2`
+      }`}>
       <Image
         src={profileImg}
         alt={name}
@@ -27,8 +34,18 @@ const ProfileCard = ({
       />
 
       <div className="">
-        <p className="text-base font-semibold text-dark-1">{name || ""} </p>
-        <p className="text-dark-3 text-xs">@{username || ""}</p>
+        <p
+          className={`text-base font-semibold ${
+            pathname === `/${username}` ? `text-light-1` : `text-dark-1`
+          }`}>
+          {name || ""}{" "}
+        </p>
+        <p
+          className={`${
+            pathname === `/${username}` ? `text-light-4` : `text-dark-3`
+          } text-xs`}>
+          @{username || ""}
+        </p>
       </div>
     </div>
   );
