f4c5dec9988d58760d5264a1efa6e64d6e6cfcf7
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-21 13:27:54 +0800

    showject info page updated

--- app/(root)/showject/[showjectId]/page.tsx
+++ app/(root)/showject/[showjectId]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchShowjectInfo } from "@/lib/actions/showject.action";
 import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const page = async ({ params }: { params: { showjectId: string } }) => {
@@ -11,7 +12,23 @@ const page = async ({ params }: { params: { showjectId: string } }) => {
 
   return (
     <section className="flex-1 min-h-screen bg-light-2 p-3">
-      <div className="max-w-[800px] mx-auto px-3 py-10"></div>
+      <div className="max-w-[800px] mx-auto px-3 py-10">
+        <Image
+          src={showjectInfo.showjectImg}
+          alt={`${showjectInfo.title}'s pic`}
+          width={700}
+          height={700}
+          className="w-full max-h-[400px] rounded-lg bg-light-3 object-contain mb-3"
+        />
+
+        <h2 className="text-2xl font-bold text-dark-1 mb-2">
+          {showjectInfo.title}
+        </h2>
+
+        <p className="text-sm text-dark-3 font-medium">
+          {showjectInfo.description}
+        </p>
+      </div>
     </section>
   );
 };
