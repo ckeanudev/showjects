84b090425c23edb64fbe1812881d19c854d95b48
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-27 18:09:37 +0800

    link to home

--- components/shared/LeftNavbar.tsx
+++ components/shared/LeftNavbar.tsx
@@ -25,13 +25,15 @@ const LeftNavbar = ({
 }: Props) => {
   return (
     <section className="sticky top-0 left-0 h-screen bg-light-1 p-6 w-[320px] flex flex-col">
-      <Image
-        src="/images/logo-light.svg"
-        alt="showjects logo"
-        width={140}
-        height={140}
-        className="w-[130px] h-auto"
-      />
+      <Link href="/home">
+        <Image
+          src="/images/logo-light.svg"
+          alt="showjects logo"
+          width={140}
+          height={140}
+          className="w-[130px] h-auto"
+        />
+      </Link>
 
       <div className="border-y-[1px] mt-5 py-5 flex flex-col justify-between gap-10 flex-1 overflow-auto">
         <div className="flex flex-col gap-4">
