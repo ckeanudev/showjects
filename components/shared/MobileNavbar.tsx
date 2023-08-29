1f9fdcea3574996be94aefe1e822cd21fbac5521
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-29 18:29:46 +0800

    mobile navbar added learn page

--- components/shared/MobileNavbar.tsx
+++ components/shared/MobileNavbar.tsx
@@ -1,7 +1,10 @@
-// "use client";
-import { MdAddCircle, MdPersonSearch, MdHome } from "react-icons/md";
+import {
+  MdAddCircle,
+  MdPersonSearch,
+  MdHome,
+  MdMenuBook,
+} from "react-icons/md";
 import { BsFillCollectionFill, BsPersonCircle } from "react-icons/bs";
-import Link from "next/link";
 import MobileNavLinks from "./MobileNavLinks";
 
 interface Props {
@@ -12,27 +15,32 @@ const MobileNavbar = ({ currentUsername }: Props) => {
   return (
     <section className="fixed bottom-0 left-0 z-10 w-full bg-light-1 border-t  flex md:hidden items-center justify-between px-5 py-3">
       <MobileNavLinks href="/">
-        <MdHome size={24} />
+        <MdHome size={21} />
         <p className="hidden sm:flex text-xs">Home</p>
       </MobileNavLinks>
 
       <MobileNavLinks href="/create-showject">
-        <MdAddCircle size={24} />
+        <MdAddCircle size={21} />
         <p className="hidden sm:flex text-xs">Create</p>
       </MobileNavLinks>
 
       <MobileNavLinks href="/top-showjects">
-        <BsFillCollectionFill size={24} />
+        <BsFillCollectionFill size={21} />
         <p className="hidden sm:flex text-xs">Showjects</p>
       </MobileNavLinks>
 
       <MobileNavLinks href="/search-developers">
-        <MdPersonSearch size={24} />
+        <MdPersonSearch size={21} />
         <p className="hidden sm:flex text-xs">Search</p>
       </MobileNavLinks>
 
+      <MobileNavLinks href="/learn">
+        <MdMenuBook size={21} />
+        <p className="hidden sm:flex text-xs">Learn</p>
+      </MobileNavLinks>
+
       <MobileNavLinks href={`/${currentUsername}`}>
-        <BsPersonCircle size={24} />
+        <BsPersonCircle size={21} />
         <p className="hidden sm:flex text-xs">Profile</p>
       </MobileNavLinks>
     </section>
