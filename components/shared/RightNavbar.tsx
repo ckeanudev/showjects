8e10c1c8df3c055252ee3448d478cfa30cf7d882
Ckeanu Richer Locson <86586510+ckeanudev@users.noreply.github.com>
2023-08-29 15:59:39 +0800

    Right navbar fixed

--- components/shared/RightNavbar.tsx
+++ components/shared/RightNavbar.tsx
@@ -12,41 +12,42 @@ const RightNavbar = async ({ authUserId, dbUserId, followingList }: Props) => {
   const allUser = await fetchAllUsers(dbUserId);
 
   return (
-    <section className="hidden xl:flex sticky top-0 right-0 h-screen bg-light-1 py-6 px-4 w-[280px] flex-col gap-1">
-      <div className="flex-1 p-1">
+    <section className="hidden xl:grid grid-cols-1 grid-rows-2 sticky top-0 right-0 h-screen bg-light-1 py-6 px-4 w-[280px] flex-col gap-1">
+      <div className="p-1 flex flex-col">
         <h2 className="font-semibold text-dark-1 mb-3">Suggested developers</h2>
 
-        <div>
+        <ScrollArea className="flex-1 overflow-auto">
           {allUser.length > 0 ? (
             allUser.map((data, i: number) => {
               return (
                 !(data.followers.indexOf(dbUserId) > -1) &&
-                i < 4 && <RightBarCard dataUser={data} />
+                i < 10 && <RightBarCard dataUser={data} />
               );
             })
           ) : (
             <p>No developers</p>
           )}
-        </div>
+        </ScrollArea>
       </div>
 
-      <div className="flex-1 p-1">
+      <div className="p-1 flex flex-col">
         <h2 className="font-semibold text-dark-1 mb-3">
           Developers you follow
         </h2>
 
-        <div className="overflow-auto">
+        <ScrollArea className="flex-1 overflow-auto">
           {allUser.length > 0 ? (
             allUser.map((data: any, i: number) => {
               return (
-                data.followers.indexOf(dbUserId) > -1 &&
-                i < 4 && <RightBarCard dataUser={data} />
+                data.followers.indexOf(dbUserId) > -1 && (
+                  <RightBarCard dataUser={data} />
+                )
               );
             })
           ) : (
             <p>No developers</p>
           )}
-        </div>
+        </ScrollArea>
       </div>
     </section>
   );
